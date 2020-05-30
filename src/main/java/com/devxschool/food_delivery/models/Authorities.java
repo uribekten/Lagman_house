5e8a78e1b07d77c198183b9cba9d670d137c6b00
Mirlan Ipasov <mipasov@gmail.com>
2020-05-30 23:13:17 +0600

    custom user details service + jdbc data store

---
+++ src/main/java/com/devxschool/food_delivery/models/Authorities.java
@@ -0,0 +1,30 @@
+package com.devxschool.food_delivery.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Authorities implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    String authority;
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
